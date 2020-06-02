9469811923267198798b7e812d14d88c698b970b
MarcoAOliari <marcoaoliari@gmail.com>
2020-06-02 20:12:37 -0300

    Dia 2

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,14 @@
 import express from 'express'
+import cors from 'cors'
+import path from 'path'
+import routes from './routes'
 
 const app = express()
 
-app.get('/', (req, res) => {
+app.use(cors())
+app.use(express.json())
+app.use(routes)
 
-})
-
-app.get('/users', (req, res) => {
-    res.json({
-        "name": "Marco",
-        "surname": "Cleito"
-    })
-})
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 
 app.listen(3333)
